c44f0c1df135cf2c3bf524e5a5377df49880cc4e
JoreKut <37114216+JoreKut@users.noreply.github.com>
2021-11-12 12:31:47 +0300

    "Multithreading download has been integrated"

--- main.py
+++ main.py
@@ -1,35 +1,42 @@
 import json
 import os
-
+import threading
+import time
 import requests
 import vk_api
-from SETTING import *
 import datetime
+
+from SETTING import *
+
 
 global photo_index
 global news_index
 global today
 
 
-def getToday():
-    return datetime.date.today()
-
-
 def link_handler(media_link_row):
-    photo_media_row = media_link_row['photo']['sizes']
-    url = media_link_row['url']
 
-    desired_photo = max(photo_media_row, key=lambda x: x['height'])
+    photo_url = ''
 
-    link_news.append(url)
-    photo_news.append(desired_photo['url'])
+    if 'photo' in media_link_row:
+        photo_media_row = media_link_row['photo']['sizes']
+
+        desired_photo = max(photo_media_row, key=lambda x: x['height'])
+        photo_url = desired_photo['url']
+
+    link = media_link_row['url']
+
+    return photo_url, link
 
 
 def image_handler(media_image_row):
 
     photo_media_row = media_image_row['sizes']
+
+    # Find the best photo
     desired_photo = max(photo_media_row, key=lambda x: x['height'])
-    photo_news.append(desired_photo['url'])
+
+    return desired_photo['url']
 
 
 def row_handler(news_row):
@@ -42,62 +49,73 @@ def row_handler(news_row):
     group_name = vk.groups.getById(group_id=source_id)[0]['name']
 
     if media_type == 'link':
-        owner_news.append(group_name)
-        text_news.append(news_row['text'])
-        link_handler(media_row)
+        owner = group_name
+        text = news_row['text']
+        photo_url, link = link_handler(media_row)
+
+        load_news(owner, text, photo_url, link)
 
     elif media_type == 'photo' or media_type == 'album':
-        owner_news.append(group_name)
-        text_news.append(news_row['text'])
-        link_news.append('')
-        image_handler(media_row)
+        owner = group_name
+        text = news_row['text']
+        photo_url = image_handler(media_row)
+        link = ''
+        load_news(owner, text, photo_url, link)
 
 
-owner_news = []
-text_news = []
-photo_news = []
-link_news = []
-
-
-def load_data():
+def load_news(owner, text, photo_url, link):
 
     global photo_index, news_index
 
-    for owner, text, photo_url, link in zip(owner_news, text_news, photo_news, link_news):
+    # Creating the folder for current news
+    os.makedirs(f'NEWS/{today}/{news_index}/', exist_ok=True)
 
-        os.makedirs(f'NEWS/{today}/{news_index}/', exist_ok=True)
-
+    # Photo loading
+    if photo_url != '':
         image = requests.get(photo_url).content
-
-        with open(f'NEWS/{today}/{news_index}/news.txt', 'w', encoding='utf-8') as file:
-            j = json.dumps({'owner : ': owner, 'text : ': text, 'link : ': link}, indent=4, ensure_ascii=False,)
-            file.write(j)
-
         with open(f'NEWS/{today}/{news_index}/{photo_index}_download_photo.png', 'wb') as file:
             file.write(image)
             photo_index += 1
 
-        news_index += 1
+    # JSON loading
+    with open(f'NEWS/{today}/{news_index}/news.json', 'w', encoding='utf-8') as file:
+        j = json.dumps({'owner : ': owner, 'text : ': text, 'link : ': link}, indent=4, ensure_ascii=False,)
+        file.write(j)
+
+    # Incrementing of news index
+    news_index += 1
 
 
 if __name__ == '__main__':
 
+    # Initialization
     photo_index = 1
     news_index = 1
-    today = getToday()
+    counter = 1
+    media_types = ['photo', 'album', 'link']
+    threads_list = []
 
+    start_time = time.time()
+    today = datetime.date.today()
+
+    # Creating "TODAY" path directory
     os.makedirs(f'NEWS/{today}/', exist_ok=True)
 
+    # VK-authorization
     vk_session = vk_api.VkApi(login, password)
     vk_session.auth()
     vk = vk_session.get_api()
 
-    data = vk.newsfeed.get(filters='post', fields='firstname')
+    # Getting newsfeed
+    data = vk.newsfeed.get(filters='post')
 
     for row in data['items']:
         # Если прусутствует медиа файлы
         if 'attachments' in row:
-            row_handler(row)
-
-    print('###')
-    load_data()
+            print(counter)
+            row_type = row['attachments'][0]['type']
+            if row_type in media_types:
+                counter += 1
+                thread = threading.Thread(target=row_handler, args=(row,))
+                thread.start()
+                threads_list.append(thread)
